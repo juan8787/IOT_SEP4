e3e7bc4b0bc8a4a0a187e4dc4a336226ae2e3e5f
Juan Vizcaino Martin <1987jvm@gmail.com>
2023-05-10 08:56:49 +0000

    jdjdjdj

--- test/src/hello_test.cpp
+++ test/src/hello_test.cpp
@@ -2,16 +2,18 @@
 #include "fff/fff.h"
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(HelloTest, BasicAssertions)
+{
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
 
-TEST(HelloTest2, BasicAssertions) {
+TEST(HelloTest2, BasicAssertions)
+{
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_EQ(7 * 6, 4);
 }
